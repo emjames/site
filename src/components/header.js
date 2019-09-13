c76db2b4fa456035375c9ef8c97c0c9f64c49b25
ej <ej@emjames.com>
2019-09-13 18:57:00 +0800

    Use Header as navigation in site

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,29 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import Image from "../components/mainImage"
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, marginTop: `2rem` }}>
+      <Link to="/">
+        <Image />
+      </Link>
+
+      <ThemeToggler>
+        {({ theme, toggleTheme }) => (
+          <label>
+            <input
+              type="checkbox"
+              onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+              checked={theme === 'dark'}
+            />{' '}
+            Dark Mode
+          </label>
+        )}
+      </ThemeToggler>
+
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,9 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/mainImage"
 import SEO from "../components/seo"
 import Contact from "../components/contact"
 import Header from "../components/header"
-import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -23,21 +21,6 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Home" />
       <Header siteTitle={title}/>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, marginTop: `2rem` }}>
-        <Image />
-        <ThemeToggler>
-          {({ theme, toggleTheme }) => (
-            <label>
-              <input
-                type="checkbox"
-                onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
-                checked={theme === 'dark'}
-              />{' '}
-              Dark Mode
-            </label>
-          )}
-        </ThemeToggler>
-      </div>
       <h1>{title}</h1>
       <Contact />
     </Layout>
