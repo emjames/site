6d7273dcbcc828d51a9d72d07d27cb0791dd92d3
ej <ej@emjames.com>
2019-09-13 17:40:00 +0800

    Use ThemeToggler to toggle dark mode

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Contact from "../components/contact"
+import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -21,6 +22,18 @@ const IndexPage = () => {
       <SEO title="Home" />
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem`, marginTop: `2rem` }}>
         <Image />
+        <ThemeToggler>
+          {({ theme, toggleTheme }) => (
+            <label>
+              <input
+                type="checkbox"
+                onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
+                checked={theme === 'dark'}
+              />{' '}
+              Dark Mode
+            </label>
+          )}
+        </ThemeToggler>
       </div>
       <h1>{data.site.siteMetadata.title}</h1>
       <Contact />
